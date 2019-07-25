ce2b23c4643b3599a18c2bf9407e8549dff21439
Nick Engmann <cyengmann@gmail.com>
2019-07-24 21:26:27 -0700

    Regex Is Fun

--- popover/popover-imbed.js
+++ popover/popover-imbed.js
@@ -1,27 +1,35 @@
 
-// console.log("we in this?")       
-// try {
-//     console.log("comon fam")
-
-//     var testTarget = parentDOM.getElementsByClassName("test")[0]; // the first element, as we wanted
-//     console.log(testTarget); //<p class="test">hello world 2</p>
-// }
-// catch(error) {
-//     // console.error(error);
-//     // expected output: ReferenceError: nonExistentFunction is not defined
-//     // Note - error messages will vary depending on browser
-// }
-
-
-        
 $().ready(() => {      
-    console.log("content script ready");
+
     var parentDOM = document.getElementById("spc-orders");
-    console.log(parentDOM)
-    // "a-row shipping-speed ship-option pointer no-scheduled-delivery a-touch-radio"
-    var test = parentDOM.getElementsByClassName("shipping-speed ship-option pointer")
-    console.log(test); 
-    for( var i = 0; i < test.length; i++) { 
-      console.log(i)
+    // this level of DOM doesn't get the Pickup a Amazon Delivery Stop
+    // var innerDOM = parentDOM.getElementsByClassName("shipping-speed ship-option pointer")
+    var innerDOM = parentDOM.getElementsByClassName("a-label a-radio-label")
+
+    for( var index = 0; index < innerDOM.length; index++ ) {
+      console.log(index)
+
+      var dates = innerDOM[index].childNodes[0].nextElementSibling.childNodes[1].innerText
+      console.log(dates);
+
+      // TODO: Regex to get the monetary amount
+      var everything = innerDOM[index].childNodes[1].innerText.toString()
+      var noDatesRe = /[$](.*)/gm
+      var noDatesArray = everything.match(noDatesRe);
+      var noDates = noDatesArray[0].toString()
+      var onlyMoneyRe = /^[$]+[0-9]+(\.[0-9]{1,2})?/gm;
+      var monetaryAmountArray = noDates.match(onlyMoneyRe);
+      var monetaryAmount = monetaryAmountArray[0]
+      console.log(monetaryAmount);
+
+      var onlyDelivery = /\-(.*)?/gm;
+      var onlyDeliveryArray = noDates.match(onlyDelivery);
+      if(onlyDeliveryArray) {
+        var onlyDelivery = onlyDeliveryArray[0].substr(2)
+        console.log(onlyDelivery);
+        // objects.delivery = onlyDelivery
+      }
+      console.log("")
+
     }
 });
